c3760d99f8bbd4f4f18da926101767d363b191ed
TiboMessiaen <tibo.messiaen@student.howest.be>
2025-02-27 14:17:40 +0100

    Feat: added contact form

--- landing-page-template/src/components/Contact.tsx
+++ landing-page-template/src/components/Contact.tsx
@@ -1,20 +1,28 @@
 export default function Contact() {
     return (
-        <section id="Contact" className="py-20 bg-white  text-center">
-            <h2 className="text-3xl font-bold mb-6">Contact</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-6">
-                <div>
-                    <h3 className="text-xl font-semibold">Email</h3>
-                    <p className="text-gray-600">Send us an email at
-                        <a href="mailto:
-                    [email protected]"> [email protected]</a>
-                    </p>
+        <section id="Contact" className="py-20 bg-white">
+            <h2 className="text-3xl font-bold mb-6 text-center">Contact</h2>
+            <form action="submit" className="flex flex-col p-6 gap-4 mx-auto rounded-md bg-gray-50 max-w-md md:max-w-xl lg:max-w-4xl lg:gap-8 lg:p-8 ">
+                <div className="flex flex-col md:flex-row justify-between items-center md:gap-4">
+                    <div className="flex flex-col w-full md:w-1/2 mb-4 md:mb-0">
+                        <label htmlFor="firstName" className="mb-2 font-medium">FirstName</label>
+                        <input type="text" id="firstName" name="firstName" placeholder="Jhon" className="p-2 border border-gray-300 rounded" />
+                    </div>
+                    <div className="flex flex-col w-full md:w-1/2 mb-4 md:mb-0">
+                        <label htmlFor="lastName" className="mb-2 font-medium">LastName</label>
+                        <input type="lastName" id="lastName" name="lastName" placeholder="Doe" className="p-2 border border-gray-300 rounded" />
+                    </div>
                 </div>
-                <div>
-                    <h3 className="text-xl font-semibold">Phone</h3>
-                    <p className="text-gray-600">Call us at 123-456-7890.</p>
+                <div className="flex flex-col w-full">
+                    <label htmlFor="email" className="mb-2 font-medium">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Jhon.Doe@Gmail.com" className="p-2 border border-gray-300 rounded" />
                 </div>
-            </div>
+                <div className="flex flex-col w-full">
+                    <label htmlFor="message" className="mb-2 font-medium">Message</label>
+                    <textarea name="message" id="message" placeholder="Hey, I got some extra questions about the drones." className="p-2 border border-gray-300 rounded"></textarea>
+                </div>
+                <button className="px-6 py-3  bg-blue-600 text-white rounded-lg font-medium">Submit</button>
+            </form>
         </section>
     );
 }
